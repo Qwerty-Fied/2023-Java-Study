a272ea9c69f7f798ba38da8d13e1a0543a10779b
Qwerty-Fied <133567685+Qwerty-Fied@users.noreply.github.com>
2023-05-24 22:55:41 +0900

    Add files via upload

---
+++ hantaeyoung.java
@@ -0,0 +1,24 @@
+package quiz.someMade.Week2;
+
+
+public class hantaeyoung {
+    int minus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    String standup() {
+        System.out.println("일어납니다.");
+        return null;
+    }
+
+    String sitdown() {
+        System.out.println("앉습니다.");
+        return null;
+    }
+
+    int divide(int x, int y) {
+        int result = (int) x / (int) y;
+        return result;
+    }
+}
