fbabf52d326b6f12941b6e10388f9628571f1ed0
Qwerty-Fied <133567685+Qwerty-Fied@users.noreply.github.com>
2023-05-24 22:44:30 +0900

    Add files via upload

---
+++ wkdeogus.java
@@ -0,0 +1,15 @@
+package quiz.someMade.Week2;
+public class wkdeogus {
+
+    String university;
+    String department;
+
+    String schoolYear;
+
+    public wkdeogus(String university, String department, String schoolYear){
+            this.university = university;
+            this.department = department;
+            this.schoolYear = schoolYear;
+
+    }
+}
